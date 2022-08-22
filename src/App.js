1c3427652229fc349874c756ab7a19a7cebfa988
Dmitry <bigdaddytrape@gmail.com>
2022-08-22 19:34:15 +0300

    get data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onFetch = this.onFetch.bind(this);
+  }
+  onFetch() {
+    fetch(`https://spapi.dev/api/characters/2`)
+      .then((pr) => pr.json())
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button className="App-link" onClick={this.onFetch}>
+            press me!
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
