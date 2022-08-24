fc56e7c0f4f08b87037fd72f55c786e6b95be511
Dmitry <samohin1dmitry@gmail.com>
2022-08-24 17:43:08 +0300

    static pers card

--- src/components/charactersList/charactersList.js
+++ src/components/charactersList/charactersList.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import GetData from "../../getData/getData";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./charactersList.sass";
 
 export default class CharactersList extends Component {
   constructor(props) {
     super(props);
+    this.f();
   }
+
+  state = {
+    name: "123",
+    age: 12,
+  };
+
+  GetData = new GetData();
+
+  f() {
+    this.GetData.fetchRequest().then((data) => {
+      this.setState({ name: data.data.name, age: data.data.age });
+    });
+  }
+
   render() {
-    return <></>;
+    const { name, age } = this.state;
+    return (
+      <div className="card">
+        <div>{name}</div>
+        <div>{age}</div>
+        <button>press</button>
+      </div>
+    );
   }
 }
--- src/getData/getData.js
+++ src/getData/getData.js
@@ -1,27 +1,6 @@
-import React, { Component } from "react";
-import { Button } from "reactstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./getData.sass";
-
-export default class GetData extends Component {
-  constructor(props) {
-    super(props);
-    this.fetchRequest = this.fetchRequest.bind(this);
-  }
-  fetchRequest() {
-    fetch(`https://spapi.dev/api/characters/2`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.data);
-      });
-  }
-  render() {
-    return (
-      <div>
-        <Button color="primary" onClick={this.fetchRequest}>
-          Click Me
-        </Button>
-      </div>
-    );
+export default class GetData {
+  async fetchRequest() {
+    let res = await fetch(`https://spapi.dev/api/characters/2`);
+    return await res.json();
   }
 }
