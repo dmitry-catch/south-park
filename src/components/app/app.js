432f681f8cae898629c828651ce555abb8757f4d
Alexandr <sao.bur@yandex.ru>
2022-09-17 15:06:44 +0300

    remove contructor

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,43 +1,39 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import ItemList from "../itemList";
+import ItemList from '../itemList';
 
-import Char from "../char";
-import CharList from "../charList";
+import Char from '../char';
+import CharList from '../charList';
 
-import Familie from "../familie";
-import FamilieList from "../familieList";
+import Familie from '../familie';
+import FamilieList from '../familieList';
 
-import Location from "../location";
-import LocationList from "../locationList";
+import Location from '../location';
+import LocationList from '../locationList';
 
-import Header from "../header";
+import Header from '../header';
 
-import "./app.sass";
+import './app.sass';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <Router>
-        <div className="app">
+        <div className='app'>
           <Header />
           <h1>South park</h1>
           <Routes>
             <Route
-              path="/chars"
+              path='/chars'
               element={<ItemList item={<Char />} list={<CharList />} />}
             />
             <Route
-              path="/families"
+              path='/families'
               element={<ItemList item={<Familie />} list={<FamilieList />} />}
             />
             <Route
-              path="/locations"
+              path='/locations'
               element={<ItemList item={<Location />} list={<LocationList />} />}
             />
           </Routes>
